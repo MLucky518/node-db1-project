f5c49bc4f8c9ceeb70f46ca9a56df06358db99f9
MLucky518 <lucky32815@gmail.com>
2020-04-26 15:50:36 -0400

    mvp

--- api/server.js
+++ api/server.js
@@ -6,4 +6,77 @@ const server = express();
 
 server.use(express.json());
 
+
+server.get("/accounts", async (req,res,next) =>{
+    try{
+        const accounts = await db("accounts").select("*");
+        res.json(accounts);
+    }catch(err){
+        next(err);
+    }
+});
+
+server.get("/accounts/:id", async (req,res,next) =>{
+    try{
+        const account = await db("accounts").first().where("id",req.params.id);
+        res.json(account);
+    }catch(err){
+        next(err);
+    }
+})
+
+server.post("/accounts", async (req,res,next) =>{
+    try{
+        const payload = {
+            name:req.body.name,
+            budget:req.body.budget
+        }
+        const [id] = await db("accounts").insert(payload);
+        const account = await db("accounts").where("id",id).first();
+        res.json(account);
+
+    }catch(err){
+        next(err);
+    }
+});
+
+server.put("/accounts/:id", async (req,res,next) =>{
+    try{
+        const payload ={
+            name:req.body.name,
+            budget:req.body.budget
+        }
+
+        await db("accounts").where("id",req.params.id).update(payload);
+        const account = await db("accounts").where("id",req.params.id).first();
+        res.json(account);
+
+    }catch(err){
+        next(err);
+    }
+});
+
+server.delete("/accounts/:id", async (req,res,next) =>{
+    try{
+        await db("accounts").where("id",req.params.id).del();
+        res.status(202).json();
+    }catch(err){
+        next(err);
+    }
+});
+
+
+
+
+
+
+
+server.use((err, req, res, next) => {
+	console.log(err)
+	res.status(500).json({
+		message: "Something went wrongs",
+	})
+})
+
+
 module.exports = server;
